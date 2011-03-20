7b06645a701be4513476d8ec292e901d612346f8
grosser <grosser.michael@gmail.com>
2011-03-20 19:15:51 +0100

    basic info from bucket

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -2,4 +2,14 @@ class Bucket < ActiveRecord::Base
   def to_param
     name
   end
+
+  def keys
+    s3.list_bucket(name)
+  end
+
+  private
+
+  def s3
+    @s3 ||= RightAws::S3Interface.new(access_key_id, secret_access_key)
+  end
 end
