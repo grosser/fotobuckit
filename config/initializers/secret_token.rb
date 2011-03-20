bc88914fb7cfd361a30649191a8a8d5543d5a9a9
grosser <grosser.michael@gmail.com>
2011-03-20 18:21:04 +0100

    base app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fotobuckit::Application.config.secret_token = '8d0bc9f31cfd23541c4fd57fe9e9dc51c11e5ab918d119e613dbf2bbaef394b3330bbdff4f91a61c8d71f78ac1bb06c2c07dde926c514fc0908712588dd27e10'
