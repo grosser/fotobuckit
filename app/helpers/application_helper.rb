6a872dc2935efbee7416c4fdf88876e817134fad
grosser <grosser.michael@gmail.com>
2011-03-21 21:03:00 +0100

    add pixelcloud image resize

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,14 @@
 module ApplicationHelper
+  def image_for(file, size)
+    image_tag resized_image_url(file.url, size), :title => file.title
+  end
+
   def resized_image_url(url, size)
+    account = CGI.escape('Michael Grosser')
     width, height = size.split('x',2)
-    "http://image-resize.appspot.com/?url=#{url}&width=#{width}&height=#{height}"
+    api_key = 'a65fe903338d83e3594e1211ef272cc4'
+    url = "http://pixelcloud.duostack.net/crop/#{CGI.escape(url)}?width=#{width}&height=#{height}&account=#{account}"
+    hash = Digest::MD5.hexdigest(url + api_key)
+    url + "&hash=#{hash}"
   end
 end
--- app/models/s3_file.rb
+++ app/models/s3_file.rb
@@ -1,2 +1,11 @@
 class S3File < ActiveRecord::Base
+  belongs_to :bucket
+
+  def url
+    bucket.s3.get_link(bucket.name, key) + "&i=#{last_modified.to_i}"
+  end
+
+  def title
+    File.basename(key)
+  end
 end
