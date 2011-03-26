e2f4677a551ce14dbabb3e6c0aeaf90f34759f99
grosser <grosser.michael@gmail.com>
2011-03-26 15:05:36 +0100

    extract authentification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,4 @@
 class ApplicationController < ActionController::Base
+  include AuthenticatedSystem
   protect_from_forgery
-
-  def current_user
-    @current_user ||= User.find_by_id(session[:user_id])
-  end
-  helper_method :current_user
-
-  def current_user=(user)
-    @current_user = user
-    session[:user_id] = user.try(:id)
-  end
-
-  def login_required
-    redirect_to '/', :error => 'You need to log in!' unless current_user
-  end
 end
---
+++ app/mixins/authenticated_system.rb
@@ -0,0 +1,18 @@
+module AuthenticatedSystem
+  def self.included(base)
+    base.helper_method :current_user
+  end
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+
+  def current_user=(user)
+    @current_user = user
+    session[:user_id] = user.try(:id)
+  end
+
+  def login_required
+    redirect_to '/', :error => 'You need to log in!' unless current_user
+  end
+end
