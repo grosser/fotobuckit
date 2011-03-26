a28e81999881aa23a059b56b0eed62f9dd57fd42
grosser <grosser.michael@gmail.com>
2011-03-26 20:02:36 +0100

    fix for heroku

--- db/migrate/20110326164044_encryp_passwords.rb
+++ db/migrate/20110326164044_encryp_passwords.rb
@@ -2,7 +2,7 @@ class EncrypPasswords < ActiveRecord::Migration
   def self.up
     rename_column :users, :password, :hashed_password
     add_column :users, :salt, :string
-    User.update_all 'salt = "abcasdasdasd321231"'
+#    User.update_all 'salt = "abcasdasdasd321231"'
     change_column :users, :salt, :string, :null => false
   end
 
