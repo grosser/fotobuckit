e50b8805dbf578621132656160e575c8d1645538
grosser <grosser.michael@gmail.com>
2011-04-27 07:54:49 +0200

    update parallel_tests

--- vendor/plugins/parallel_tests/lib/parallel_specs/spec_runtime_logger.rb
+++ vendor/plugins/parallel_tests/lib/parallel_specs/spec_runtime_logger.rb
@@ -1,7 +1,18 @@
-require 'spec/runner/formatter/progress_bar_formatter'
+require 'parallel_specs'
 
-class ParallelSpecs::SpecRuntimeLogger < Spec::Runner::Formatter::BaseTextFormatter
-  def initialize(options, output)
+begin
+  require 'rspec/core/formatters/progress_formatter'
+  base = RSpec::Core::Formatters::ProgressFormatter
+rescue LoadError
+  require 'spec/runner/formatter/progress_bar_formatter'
+  base = Spec::Runner::Formatter::BaseTextFormatter
+end
+ParallelSpecs::SpecRuntimeLoggerBase = base
+
+class ParallelSpecs::SpecRuntimeLogger < ParallelSpecs::SpecRuntimeLoggerBase
+  def initialize(options, output=nil)
+    output ||= options # rspec 2 has output as first argument
+
     if String === output
       FileUtils.mkdir_p(File.dirname(output))
       File.open(output,'w'){|f| f.write ''} # clean the file
@@ -10,6 +21,7 @@ class ParallelSpecs::SpecRuntimeLogger < Spec::Runner::Formatter::BaseTextFormat
       @output = output
     end
     @example_times = Hash.new(0)
+    @failed_examples = [] # only needed for rspec 2
   end
 
   def example_started(*args)
@@ -24,7 +36,9 @@ class ParallelSpecs::SpecRuntimeLogger < Spec::Runner::Formatter::BaseTextFormat
   def start_dump(*args)
     return unless ENV['TEST_ENV_NUMBER'] #only record when running in parallel
     # TODO: Figure out why sometimes time can be less than 0
-    @output.puts @example_times.map { |file, time| "#{file}:#{time > 0 ? time : 0}" }
+    lock_output do
+      @output.puts @example_times.map { |file, time| "#{file}:#{time > 0 ? time : 0}" }
+    end
     @output.flush
   end
 
@@ -45,5 +59,19 @@ class ParallelSpecs::SpecRuntimeLogger < Spec::Runner::Formatter::BaseTextFormat
   #stolen from Rspec
   def close
     @output.close  if (IO === @output) & (@output != $stdout)
+  end
+
+  # do not let multiple processes get in each others way
+  def lock_output
+    if File === @output
+      begin
+        @output.flock File::LOCK_EX
+        yield
+      ensure
+        @output.flock File::LOCK_UN
+      end
+    else
+      yield
+    end
   end
 end
